8786584024269e7ca5a6ef7b1b8b9511a3ef1526
s14003 <s14003@std.it-college.ac.jp>
2015-08-25 13:58:50 +0900

    得点を出す画面まで遷移完了(・∀・)

---
+++ app/src/main/java/com/example/s14003/englishmaster/Finish.java
@@ -0,0 +1,56 @@
+package com.example.s14003.englishmaster;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.util.Map;
+
+
+public class Finish extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_finish);
+
+        SharedPreferences prefs
+                = getSharedPreferences("Data", Context.MODE_PRIVATE);
+         int time = prefs.getInt("TIME", 0);
+        TextView number = (TextView)findViewById(R.id.time);
+        number.setText("" + time);
+        Log.d("TIME","" + time);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_finish, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
